26a556284f51af37cae94169872ce9f69161f0b1
mbiggin <matthew.biggin@gmail.com>
2017-05-14 20:09:48 +0100

    Trying to put things back together

--- src/main/java/com/ultraflynn/sygrl/Main.java
+++ src/main/java/com/ultraflynn/sygrl/Main.java
@@ -1,10 +1,12 @@
 package com.ultraflynn.sygrl;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
--- src/main/java/com/ultraflynn/sygrl/controller/BootstrapController.java
+++ src/main/java/com/ultraflynn/sygrl/controller/BootstrapController.java
@@ -9,7 +9,6 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Controller
-@SpringBootApplication
 public class BootstrapController {
     @Value("${spring.datasource.url}")
     private String dbUrl;
